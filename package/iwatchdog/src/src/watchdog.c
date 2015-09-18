a4fa28a159f58a333e4387589c863132a0bfee81
Kenneth Johansson <kenneth@southpole.se>
2015-09-18 18:40:38 +0200

    watchdog crashlog and corelog backport from devel.

--- package/iwatchdog/src/src/watchdog.c
+++ package/iwatchdog/src/src/watchdog.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -10,6 +12,7 @@ const char *kicker        = "OK";
 
 int main(int argc, char **argv)
 {
+        pid_t pid, sid;
         int fd = open(watchdog_file, O_WRONLY);
         if (fd < 0) {
                 perror("Open watchdog file");
@@ -23,6 +26,41 @@ int main(int argc, char **argv)
                 perror("Error init watchdog");
                 exit(1);
         }
+
+	/* daemonize */
+	if (1) {
+
+		/* Fork off the parent process */
+		pid = fork();
+		if (pid < 0) {
+			exit(EXIT_FAILURE);
+		}
+		/* If we got a good PID, then
+		   we can exit the parent process. */
+		if (pid > 0) {
+			exit(EXIT_SUCCESS);
+		}
+
+		/* Change the file mode mask */
+		umask(0);
+
+		/* Create a new SID for the child process */
+		sid = setsid();
+		if (sid < 0) {
+			/* Log the failure */
+			exit(EXIT_FAILURE);
+		}
+
+		/* Change the current working directory */
+		if ((chdir("/")) < 0) {
+			/* Log the failure */
+			exit(EXIT_FAILURE);
+		}
+		/* Close out the standard file descriptors */
+		close(STDIN_FILENO);
+		close(STDOUT_FILENO);
+		close(STDERR_FILENO);
+	}
 
         while (1) {
                 sleep(1);
