fc94216dde6876b54a14dfd366d054a0d2078fc5
Martin Schröder <mkschreder.uk@gmail.com>
2015-11-16 14:04:31 +0100

    fix iop_get_feeds

--- iop_get_feeds.sh
+++ iop_get_feeds.sh
@@ -3,5 +3,6 @@
 ./scripts/feeds uninstall asterisk18
 ./scripts/feeds install -f -p juci -a
 ./scripts/feeds install -f -p intenoprivate
-./scripts/feeds install -p intenoprivate brcm63xx_ref_nand
-./scripts/feeds install -p intenoprivate brcm63xx_ref_nand_arm
+./scripts/feeds install -f -p intenotargets
+./scripts/feeds install -p intenotargets brcm63xx_ref_nand
+./scripts/feeds install -p intenotargets brcm63xx_ref_nand_arm
