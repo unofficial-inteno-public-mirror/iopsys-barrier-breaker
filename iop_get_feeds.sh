106b275ef966ad5d51859b1deccfd24423af9931
Sukru Senli <sukru.senli@inteno.se>
2015-11-26 17:35:24 +0100

    iop_get_feeds.sh update

--- iop_get_feeds.sh
+++ iop_get_feeds.sh
@@ -3,6 +3,7 @@
 ./scripts/feeds uninstall asterisk18
 ./scripts/feeds install -f -p juci -a
 ./scripts/feeds install -f -p intenoprivate -a
-./scripts/feeds install -f -p intenotargetsi -a
-./scripts/feeds install -p intenotargets brcm63xx_ref_nand
-./scripts/feeds install -p intenotargets brcm63xx_ref_nand_arm
+./scripts/feeds install -f -p intenotargets -a
+./scripts/feeds install -f -p inteno -a
+./scripts/feeds install -f -p intenotargets brcm63xx_ref_nand
+./scripts/feeds install -f -p intenotargets brcm63xx_ref_nand_arm
