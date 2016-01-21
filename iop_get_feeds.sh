b1f9e863d41cb9d44c3dce7c1a86204ea4c5cc00
Martin Schröder <mkschreder.uk@gmail.com>
2016-01-21 12:08:17 +0100

    fix feeds so that openwrt feeds are installed last (after inteno) because otherwise we get all packages wrong!

--- iop_get_feeds.sh
+++ iop_get_feeds.sh
@@ -1,13 +1,13 @@
 ./scripts/feeds update
-./scripts/feeds install -a 
-./scripts/feeds uninstall asterisk18
-./scripts/feeds uninstall libmicroxml
-./scripts/feeds uninstall libqrencode
-./scripts/feeds uninstall zstream
-./scripts/feeds uninstall lighttpd
+#./scripts/feeds uninstall asterisk18
+#./scripts/feeds uninstall libmicroxml
+#./scripts/feeds uninstall libqrencode
+#./scripts/feeds uninstall zstream
+#./scripts/feeds uninstall net-snmp
 ./scripts/feeds install -f -p juci -a
 ./scripts/feeds install -f -p intenoprivate -a
 ./scripts/feeds install -f -p intenotargets -a
 ./scripts/feeds install -f -p inteno -a
 ./scripts/feeds install -f -p intenotargets brcm63xx_ref_nand
 ./scripts/feeds install -f -p intenotargets brcm63xx_ref_nand_arm
+./scripts/feeds install -a 
