1284d35a9dd4219d49fff8fd840062f0868d54c4
Sukru Senli <sukru.senli@inteno.se>
2016-01-21 17:37:53 +0100

    feeds uninstall asterisk18 and zstream

--- iop_get_feeds.sh
+++ iop_get_feeds.sh
@@ -1,8 +1,6 @@
 ./scripts/feeds update
-#./scripts/feeds uninstall asterisk18
 #./scripts/feeds uninstall libmicroxml
 #./scripts/feeds uninstall libqrencode
-#./scripts/feeds uninstall zstream
 #./scripts/feeds uninstall net-snmp
 ./scripts/feeds install -f -p juci -a
 ./scripts/feeds install -f -p intenoprivate -a
@@ -11,3 +9,5 @@
 ./scripts/feeds install -f -p intenotargets brcm63xx_ref_nand
 ./scripts/feeds install -f -p intenotargets brcm63xx_ref_nand_arm
 ./scripts/feeds install -a 
+./scripts/feeds uninstall asterisk18
+./scripts/feeds uninstall zstream
