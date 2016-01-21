64b9917272775c53949e9d1a8cda2cafebd87198
Kenneth Johansson <kenneth@southpole.se>
2016-01-21 11:14:59 +0100

    Generate .git_update file.
    Used by update_git script to decect updates to git archives.

--- include/package.mk
+++ include/package.mk
@@ -59,6 +59,7 @@ include $(INCLUDE_DIR)/package-dumpinfo.mk
 include $(INCLUDE_DIR)/package-ipkg.mk
 include $(INCLUDE_DIR)/package-bin.mk
 include $(INCLUDE_DIR)/autotools.mk
+include $(INCLUDE_DIR)/inteno.mk
 
 override MAKEFLAGS=
 CONFIG_SITE:=$(INCLUDE_DIR)/site/$(REAL_GNU_TARGET_NAME)
