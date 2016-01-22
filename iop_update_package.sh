d5966ed613808deb20dec858ee76396b2cd09a86
Jonas Höglund <jonas.hoglund@embeddednation.com>
2016-01-22 09:58:46 +0100

    Remove old update package

--- iop_update_package.sh
+++
@@ -1,6 +0,0 @@
-cd $1
-NEW_HASH=`git log -n 1 master --pretty=format:"%H"`
-echo $NEW_HASH
-cd ../../..
-sed -r -i  "s/^PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=$NEW_HASH/g" $3
-git commit $3 -m "$2: update package"
