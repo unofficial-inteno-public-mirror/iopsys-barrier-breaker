2aef99c4f00b22f831fdd391ae732c33c9225660
Nabeel Sowan <nabeel@vibes.se>
2016-03-15 18:28:21 +0100

    fix ssh_sysupgrade scripts

--- ssh_sysupgrade_latest_ubifs.sh
+++ ssh_sysupgrade_latest_ubifs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 { cd `dirname $0`
-IMAGE=`ls -Art bin/brcm63xx_ref_nand/*.y | tail -n1`
+IMAGE=`ls -Art bin/*/*.y | tail -n1`
 [ "$IMAGE" ] && [ -e "$IMAGE" ] && ./ssh_sysupgrade.sh $1 $IMAGE $2
 }
